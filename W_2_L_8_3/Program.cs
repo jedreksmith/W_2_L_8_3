0645014f7e5aa32409708dc92d81ccec3e81fea7
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-28 22:09:49 +0200

    Add project files.

---
+++ W_2_L_8_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace W_2_L_8_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a= 1;
+
+            int b= 0;
+
+
+            Console.WriteLine("Ciąg Fibonachiego");
+
+            for (int i = 0; i < 20; i++) 
+            { 
+                Console.WriteLine(b);
+                b += a;
+                a = b - a;
+            }
+
+        }
+    }
+}
